7bc2ed9369af7107885320d797b2f51e6d5966ae
Maksym <maxim.golya@gmail.com>
2026-03-20 17:30:02 +0200

    added requested changes

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,39 +11,34 @@ function transformStateWithClones(state, actions) {
   const result = [];
 
   for (const action of actions) {
+    const newState = { ...currentState };
+
     switch (action.type) {
+      case 'default': {
+        break;
+      }
+
       case 'clear': {
-        const newState = { ...currentState };
-
         Object.keys(newState).forEach((key) => delete newState[key]);
-
-        result.push(newState);
-        currentState = newState;
         break;
       }
 
       case 'addProperties': {
-        const newState = { ...currentState };
-
         for (const key in action.extraData) {
           newState[key] = action.extraData[key];
         }
-        result.push(newState);
-        currentState = newState;
         break;
       }
 
       case 'removeProperties': {
-        const newState = { ...currentState };
-
         for (const key of action.keysToRemove) {
           delete newState[key];
         }
-        result.push(newState);
-        currentState = newState;
         break;
       }
     }
+    result.push(newState);
+    currentState = newState;
   }
 
   return result;
